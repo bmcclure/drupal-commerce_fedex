d703056269d55bae1d399ea3be161496a7282ffe
Ben McClure <ben.mcclure@gmail.com>
2017-03-08 04:56:46 -0600

    Updated trait ids and fields

--- src/Plugin/Commerce/EntityTrait/PurchasableEntityDangerousGoods.php
+++ src/Plugin/Commerce/EntityTrait/PurchasableEntityDangerousGoods.php
@@ -3,31 +3,23 @@
 namespace Drupal\commerce_fedex\Plugin\Commerce\EntityTrait;
 
 use Drupal\commerce\BundleFieldDefinition;
-use Drupal\commerce\Plugin\Commerce\EntityTrait\EntityTraitBase;
 
 /**
- * Provides the "commerce_fedex_dangerous_goods" trait.
+ * Provides the "fedex_dangerous" trait.
  *
  * @CommerceEntityTrait(
- *   id = "commerce_fedex_dangerous_goods",
+ *   id = "fedex_dangerous",
  *   label = @Translation("FedEx: Dangerous goods"),
  *   entity_types = {"commerce_product_variation"}
  * )
  */
-class PurchasableEntityDangerousGoods extends EntityTraitBase {
+class PurchasableEntityDangerousGoods extends PurchasableEntityHazardousBase {
 
   /**
    * {@inheritdoc}
    */
   public function buildFieldDefinitions() {
-    $fields = [];
-
-    $fields['commerce_fedex_dangerous_goods'] = BundleFieldDefinition::create('boolean')
-      ->setLabel('FedEx: Dangerous goods')
-      ->setDisplayOptions('form', [
-        'type' => 'boolean_checkbox',
-        'weight' => 95,
-      ]);
+    $fields = $this->baseFields();
 
     return $fields;
   }
--- src/Plugin/Commerce/EntityTrait/PurchasableEntityDryIce.php
+++ src/Plugin/Commerce/EntityTrait/PurchasableEntityDryIce.php
@@ -6,10 +6,10 @@ use Drupal\commerce\BundleFieldDefinition;
 use Drupal\commerce\Plugin\Commerce\EntityTrait\EntityTraitBase;
 
 /**
- * Provides the "commerce_fedex_dry_ice" trait.
+ * Provides the "fedex_dry_ice" trait.
  *
  * @CommerceEntityTrait(
- *   id = "commerce_fedex_dry_ice",
+ *   id = "fedex_dry_ice",
  *   label = @Translation("FedEx: Dry ice"),
  *   entity_types = {"commerce_product_variation"}
  * )
@@ -22,11 +22,23 @@ class PurchasableEntityDryIce extends EntityTraitBase {
   public function buildFieldDefinitions() {
     $fields = [];
 
-    $fields['commerce_fedex_dry_ice'] = BundleFieldDefinition::create('boolean')
-      ->setLabel('FedEx: Dry ice')
+    $id = $this->getPluginId();
+    $label = $this->getLabel();
+
+    $fields[$id] = BundleFieldDefinition::create('boolean')
+      ->setLabel($label)
       ->setDisplayOptions('form', [
         'type' => 'boolean_checkbox',
         'weight' => 95,
+      ]);
+
+    $fields[$id . '_weight'] = BundleFieldDefinition::create('float')
+      ->setLabel($label . ' ' . $this->t('weight'))
+      ->setDescription($this->t('Enter the weight of the dry ice in kg.'))
+      ->setDisplayOptions('form', [
+        'type' => 'number',
+        'weight' => 95,
+        'suffix' => ' kg',
       ]);
 
     return $fields;
