2012d6ac6ae96b0b07d6c950300a24cb094660f5
Ben McClure <ben.mcclure@gmail.com>
2017-03-08 23:34:45 -0600

    by bmcclure: Add event subscribers to the special services submodules

---
+++ modules/dangerous/src/EventSubscriber/FedExEventSubscriber.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Drupal\commerce_fedex_dangerous\EventSubscriber;
+
+use Drupal\commerce_fedex\Event\CommerceFedExEvents;
+use Drupal\commerce_fedex\Event\RateRequestEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * Class FedExEventSubscriber
+ *
+ * @package Drupal\commerce_fedex_dangerous\EventSubscriber
+ */
+class FedExEventSubscriber implements EventSubscriberInterface {
+
+  /**
+   * @param \Drupal\commerce_fedex\Event\RateRequestEvent $event
+   */
+  public function beforeRateRequest(RateRequestEvent $event) {
+    $rateRequest = $event->getRateRequest();
+
+    // TODO: Alter or replace the rate request.
+  }
+
+  /**
+   * @inheritdoc
+   */
+  static function getSubscribedEvents() {
+    $events = [];
+
+    $events[CommerceFedExEvents::BEFORE_RATE_REQUEST][] = ['beforeRateRequest'];
+
+    return $events;
+  }
+
+}
