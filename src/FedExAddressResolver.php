2be6f013f0bf81e8b3e00b9efb3efc7d91ae889b
jeremy <jeremyapplebaum12@gmail.com>
2018-01-15 15:56:51 -0600

    Add Ireland Mapping

--- src/FedExAddressResolver.php
+++ src/FedExAddressResolver.php
@@ -58,12 +58,12 @@ trait FedExAddressResolver {
    *   A Fedex Compatible party.
    */
   public static function addressResolveHK(AddressInterface $address) {
-   
+
     $party = new Party();
-    
+
     $party->setAddress(new Address(
       array_filter([$address->getAddressLine1(), $address->getAddressLine2()]),
-      $address->getAdministrativeArea().', '.$address->getLocality(),
+      $address->getAdministrativeArea() . ', ' . $address->getLocality(),
       NULL,
       $address->getPostalCode(),
       NULL,
@@ -71,12 +71,12 @@ trait FedExAddressResolver {
       NULL,
       FALSE
     ));
-    
+
     return $party;
   }
-  
+
   /**
-   * Resolve Addresses in Barbados.
+   * Resolve Addresses in Barbodos.
    *
    * @param \Drupal\address\AddressInterface $address
    *   The address to resolve.
@@ -84,10 +84,10 @@ trait FedExAddressResolver {
    * @return \NicholasCreativeMedia\FedExPHP\Structs\Party
    *   A Fedex Compatible party.
    */
-    public static function addressResolveBB(AddressInterface $address) {
- 
+  public static function addressResolveBB(AddressInterface $address) {
+
     $party = new Party();
-    
+
     $party->setAddress(new Address(
       array_filter([$address->getAddressLine1(), $address->getAddressLine2()]),
       $address->getAdministrativeArea(),
@@ -98,7 +98,66 @@ trait FedExAddressResolver {
       NULL,
       FALSE
     ));
-    
+
+    return $party;
+  }
+
+  /**
+   * Resolve Addresses in Ireland.
+   *
+   * @param \Drupal\address\AddressInterface $address
+   *   The address to resolve.
+   *
+   * @return \NicholasCreativeMedia\FedExPHP\Structs\Party
+   *   A Fedex Compatible party.
+   */
+  public static function addressResolveIE(AddressInterface $address) {
+
+    $provinces = [
+      'Co. Carlow' => 'N0',
+      'Co. Cavan' => 'N1',
+      'Co.  Clare' => 'N2',
+      'Co. Cork' => ' N3',
+      'Co.  Donegal' => 'N4',
+      'Co. Dublin' => 'N5',
+      'Co. Galway' => 'N6',
+      'Co. Kerry' => 'N7',
+      'Co. Kildare' => 'N8',
+      'Co. Kilkenny' => 'N9',
+      'Co. Laois' => 'NA',
+      'Co. Leitrim' => 'NG',
+      'Co. Limerick' => 'NI',
+      'Co. Longford' => 'NK',
+      'Co. Louth' => 'NL',
+      'Co. Mayo' => 'NM',
+      'Co. Meath' => 'NO',
+      'Co. Monaghan' => 'NP',
+      'Co. Offaly' => 'NQ',
+      'Co. Roscommon' => 'NR',
+      'Co. Sligo' => 'NW',
+      'Co. Tipperary' => 'NX',
+      'Co. Tipperary' => 'NY',
+      'Co. Waterford' => 'NZ',
+      'Co. Westmeath' => '10',
+      'Co. Wexford' => '11',
+      'Co. Wicklow' => '12',
+
+    ];
+
+
+    $party = new Party();
+
+    $party->setAddress(new Address(
+      array_filter([$address->getAddressLine1(), $address->getAddressLine2()]),
+      $address->getAdministrativeArea(),
+      $provinces[$address->getAdministrativeArea()],
+      $address->getPostalCode(),
+      NULL,
+      $address->getCountryCode(),
+      NULL,
+      FALSE
+    ));
+
     return $party;
   }
 
